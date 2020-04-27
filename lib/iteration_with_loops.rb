d342841e6f0a82814e4a712eb7591e9bf3b06df8
Daniel Reyes <daniel.c.reyes30@gmail.com>
2020-04-27 17:28:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def find_min_in_nested_arrays(src)
   row_index = 0
   while row_index <= src.count do
   element_index = 0
-  min_value = nil
+  min_value = 1000
   while element_index <= src.count do
     if min_value == nil||min_value > src[row_index][element_index]
       min_value = src[row_index][element_index]
